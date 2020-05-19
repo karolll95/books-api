39c19517edefcd456f18a9c7cfe4bca2a7e80425
krinc <karol.rinc@solwit.com>
2020-05-19 21:56:29 +0200

    Corrected access modifiers and removed driver declaration

--- src/main/java/com/karolrinc/books/api/IsbnNumberValidator.java
+++ src/main/java/com/karolrinc/books/api/IsbnNumberValidator.java
@@ -7,7 +7,7 @@ import javax.validation.ConstraintValidatorContext;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-class IsbnNumberValidator implements ConstraintValidator<IsbnNumber, String> {
+public class IsbnNumberValidator implements ConstraintValidator<IsbnNumber, String> {
     private static final String ISBN_NUMBER_REGEX =
             "^(?:ISBN(?:-1[03])?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$|97[89][0-9]{10}$|" +
             "(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)(?:97[89][- ]?)?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$";
